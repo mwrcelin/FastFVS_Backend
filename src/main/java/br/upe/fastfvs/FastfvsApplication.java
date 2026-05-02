0e078861044468b3f9c27a6e70f93c9cd19e8349
Mwrcelo <marcelo.asmoura@upe.br>
2026-05-01 22:20:13 -0300

    Iniciando Projeto

---
+++ src/main/java/br/upe/fastfvs/FastfvsApplication.java
@@ -0,0 +1,13 @@
+package br.upe.fastfvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfvsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfvsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/upe/fastfvs/FastfvsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.upe.fastfvs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastfvsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
